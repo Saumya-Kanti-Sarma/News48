44572f74b0eb5aa28c1825e4a2ddbf60fc72f7de
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-09-17 01:16:36 +0530

    added navbar

--- src/Routes/Code/C_home.jsx
+++ src/Routes/Code/C_home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Navbar from "../../Components/Navbar/Navbar.jsx"
 const C_home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       C_home
     </div>
   )
