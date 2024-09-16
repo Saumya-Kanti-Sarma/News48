f32f974566c8039f000d1c525e9c54d9c5856d17
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-09-17 00:40:40 +0530

    routes at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom'
-import C_home from './Routes/Customers/Home/C_home';
-import News from "./Routes/Customers/News/News"
-import C_publisher from './Routes/Customers/Punlisher/C_publisher';
-import C_OriginalNews from './Routes/Customers/News/Components/C_OriginalNews';
-import About from "./Routes/Customers/About/About"
-import P_Home from './Routes/Publisher/P_Home';
-import P_RegLog from "./Routes/Publisher/P_RegLog";
-import P_AddNews from "./Routes/Publisher/P_AddNews";
-import P_mypublishes from "./Routes/Publisher/P_mypublishes";
-import P_profile from "./Routes/Publisher/P_profile";
+import C_home from './Routes/Customers/Home/C_home.jsx';
+import News from "./Routes/Customers/News/News.jsx"
+import C_publisher from './Routes/Customers/Punlisher/C_publisher.jsx';
+import C_OriginalNews from './Routes/Customers/News/Components/C_OriginalNews.jsx';
+import About from "./Routes/Customers/About/About.jsx"
+import P_Home from './Routes/Publisher/P_Home.jsx';
+import P_RegLog from "./Routes/Publisher/P_RegLog.jsx";
+import P_AddNews from "./Routes/Publisher/P_AddNews.jsx";
+import P_mypublishes from "./Routes/Publisher/P_mypublishes.jsx";
+import P_profile from "./Routes/Publisher/P_profile.jsx";
 const App = () => {
   const routes = createBrowserRouter([
     // <<<<<<<<<<<<<<<<<All Publishers Routes>>>>>>>>>>>>>>>>>>>>>>>>
