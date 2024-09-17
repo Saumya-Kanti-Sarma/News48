1014f754c276fa03f8fe3ca88909dae0fe283122
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-09-17 10:58:31 +0530

    changed the component srv

--- src/Routes/Components/Navbar/Navbar.jsx
+++ src/Routes/Components/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-
+//src/components/navbar/Navbar.jsx
+import React, { useState } from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const [menu, setMenu] = useState("-100%"); // toggling the right shift of the menu
+  const [img, setImg] = useState("menu"); // toggling the menu image
+
+  function menuHandler() {
+    img == "menu" ? setImg("back") : setImg("menu");
+    menu == "-100%" ? setMenu("0") : setMenu("-100%");
+  }
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      <div className='real-nav-area'>
+        <h1><Link to={"/"}>48Hours.com</Link></h1>
+        <ul className='nav-uls'>
+          <li className='nav-links'><Link to={"/"}>Home</Link></li>
+          <li className='nav-links'><Link to={"/news"}>News</Link></li>
+          <li className='nav-links'><Link to={"/about"}>About</Link></li>
+        </ul>
+        <p>Register/Login</p>
+        <button id='menu-btn' onClick={menuHandler}>
+          <img src={`./Basic/${img}.svg`} id="menu-img" />
+        </button>
+      </div>
+      <div className="phone-menu" style={{
+        right: menu
+      }}>
+        <ul className='phone-uls'>
+          <li className='nav-links-phone'><Link to={"/"}>home</Link></li>
+          <li className='nav-links-phone'><Link to={"/news"}>News</Link></li>
+          <li className='nav-links-phone'><Link to={"/about"}>About</Link></li>
+        </ul>
+      </div>
+    </>
   )
 }
 
---
+++ src/Routes/Publisher/P_Home.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import NavBar from "../Components/Nav/P_Navbar";
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
+const P_Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const usrID = Cookies.get("usrID_");
+    if (!usrID) {
+      navigate("/publisher/login");
+    }
+  }, [])
+  return (
+    <>
+      <NavBar />
+      <h1>Welcome {Cookies.get("usrName")}</h1>
+    </>
+  )
+}
+
+export default P_Home
